b149b3d6d11e3c0ba0ea42257bb36d777b3e5b6e
sammy wlodawski <samwlodawski@gmail.com>
2024-07-20 00:20:20 -0400

    Adding sequelize gardenplants table.

--- models/gardenplants.js
+++ models/gardenplants.js
@@ -3,37 +3,35 @@ const sequelize = require('../config/connection');
 
 class gardenplants extends Model {};
 
-gardenplants.init({});
+gardenplants.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    plot_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        foreign_key: true,
+        allowNull: false
+    },
+    created_at: {
+        type: DataTypes.TIMESTAMP,
+        allowNull: false
+    },
+    updated_at: {
+        type: DataTypes.TIMESTAMP,
+        allowNull:false
+    }
+    }, {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'gardenplants'
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = gardenplants;
+    module.exports = gardenplants;
