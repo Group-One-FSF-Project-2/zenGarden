5e1c50a083e9c4e9369dc9cbaa26c107bbc83df3
mjurek1 <jurekmichael0@gmail.com>
2024-07-23 18:52:17 -0500

    pushing seed data and model updates

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,9 +1,19 @@
 const sequelize = require('../config/connection');
+
 const User = require('../models/User');
 const userData = require('../seeds/user-seeds.json');
 
+const Plant = require('../models/Plant');
+const plantData = require('../seeds/plant-seeds.json');
 
-const seedData = async () => {
+const plotPlant = require('../models/plotPlant');
+const plotPlantData = require('../seeds/plotPlant-seeds.json');
+
+const Gardenplot = require('../models/Gardenplot');
+const gardenPlotData = require('../seeds/gardenplot-seeds.json');
+
+
+const userSeedData = async () => {
     await sequelize.sync({
         force: true,
     });
@@ -16,4 +26,47 @@ const seedData = async () => {
     process.exit(0);
 };
 
-seedData();
+const plantSeedData = async () => {
+    await sequelize.sync({
+        force: true,
+    });
+
+    await Plant.bulkCreate(plantData, 
+        {individualHooks:true, 
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+const plotPlantSeedData = async () => {
+    await sequelize.sync({
+        force: true,
+    });
+
+    await plotPlant.bulkCreate(plotPlantData, 
+        {individualHooks:true, 
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+const gardenPlotSeedData = async () => {
+    await sequelize.sync({
+        force: true,
+    });
+
+    await Gardenplot.bulkCreate(gardenPlotData, 
+        {individualHooks:true, 
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+userSeedData();
+plantSeedData();
+plotPlantSeedData();
+gardenPlotSeedData();
+
