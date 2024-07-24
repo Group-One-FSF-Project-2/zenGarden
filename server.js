0e5ceee68e68d484f071ebb1379d909270324dee
swokamoto <swokamoto@gmail.com>
2024-07-24 17:15:08 -0400

    connected handlebars

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
 const controllers = require('./controllers');
-//const exhbs = require('express-handlebars');
+const exhbs = require('express-handlebars');
 
 // initialize models for sequelize
 // const model = require('./models/index');
@@ -9,27 +10,24 @@ const controllers = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//const hbs = exhbs.create({});
+const hbs = exhbs.create({});
 
-//app.engine('handlebars', hbs.engine);
-//app.set('view-engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(controllers);
 
-<<<<<<< HEAD
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log('server started');
-  });
+app.get('/', (req, res) => {
+    res.render('home');
 });
-=======
+
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
-        console.log('server started')
+        console.log('server started on http://localhost:' + PORT);
     })
 });
->>>>>>> 10961878e789c766c82c4f989c7c7035577bb599
