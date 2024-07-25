bcc9ef90731f8ffd67f198009f49ddf7bf8ba23d
smrsun4 <smrsun4@gmail.com>
2024-07-25 19:39:26 -0400

    pushing new gardenRoute code and removed duplicates

--- controllers/api/gardenplotRoutes.js
+++ controllers/api/gardenplotRoutes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const { Gardenplot, plotPlant } = require('../../models');
 // set post to pass in the user info to get the correct garden plot
+router.get('/', async (req, res) => {
+  try {
+    const gardenPlots = await Gardenplot.findAll();
+    res.status(200).json(gardenPlots);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 router.post('/', async (req, res) => {
   try {
     const newPlot = await Gardenplot.create({
@@ -17,13 +26,25 @@ router.post('/', async (req, res) => {
  // add a plant to an existing garden plot by finding it by it's id
 //  req.body needs to include: plantId  create a new plotPlant and add the plotId and the plantId to it. 
 router.put('/:id', async (req, res) => {
-    
+    const plotId = req.params.id
    try {
-    const newPlant = await plotPlant.update(req.body.plantId, {
-      
+    const [affectedRows] = await Gardenplot.update(req.body, {
+      where: {
+        plotId: plotId, 
+        plantId:req.body.plantId
+      }
     })
 
-    res.status(200).json(newPlant);
+    if(affectedRows > 0) {
+      res.status(200).json({ message: "updated successfully"});
+    }
+    
+    // const newPlant = await plotPlant.create({
+    //     plotId: plotId, 
+    //     plantId:req.body.plantId,
+ 
+    // }); 
+    // res.status(200).json(newPlant)
    } catch (err) {
     res.status(400).json(err);
    }
@@ -43,7 +64,7 @@ router.delete('/:id', async (req, res) => {
         return;
       }
   
-      res.status(200).json(plotData);
+      res.status(200).json({message: "successfully delete plot"}, plotData);
     } catch (err) {
       res.status(500).json(err);
     }
--- server.js
+++ server.js
@@ -47,24 +47,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-<<<<<<< HEAD
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => 
-    console.log(`Server started on ${PORT}.`)
- 
-  )
-});
-
-=======
->>>>>>> 75e02e27303ac4d926576c8cef7c95ea1a75a9d2
 app.use(controllers);
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-<<<<<<< HEAD
-=======
 sequelize.sync({ force: false }).then (() => {
   app.listen(PORT, () => 
     console.log('server started on http://localhost:' + PORT)
@@ -84,4 +72,3 @@ sequelize.sync({ force: false }).then (() => {
 //     });
 
 // });
->>>>>>> 75e02e27303ac4d926576c8cef7c95ea1a75a9d2
