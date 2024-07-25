e3660d64b469696dbdb473909e2cf9994c5c997f
mjurek1 <jurekmichael0@gmail.com>
2024-07-25 12:24:36 -0500

    seed data working

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ const secret = {
 app.use(session(secret));
 
 // app.engine('handlebars', hbs.engine);
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', exphbs());
+// app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
