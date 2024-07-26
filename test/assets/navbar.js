12c80c0e8935068e0ef328a2dc3b24400884c71a
MommaDLP <159870485+MommaDLP@users.noreply.github.com>
2024-07-25 19:19:46 -0500

    clean up

--- test/assets/navbar.js
+++ test/assets/navbar.js
@@ -1,28 +1,3 @@
-// const modalContainer= document.querySelector(".modal-conatiner"),
-// modalLogin = document.getElementById(".modalLogin"),
-// create = document.getElementById("create"),
-// loginHere = document.getElementById("loginHere"),
-// loginForm = document.querySelector(".login"),
-// registrationForm = document.querySelector(".registration")
-
-// modalLogin.onclick =() => {
-//     modalContainer.classList.add("open");
-// };
-
-// function closeModalContainer() {
-//     modalContainer.classList.remove("open");
-// }
-
-// create.onclick=()=>{
-//     loginForm.setAttribute("style", "transform: translate(-500px);");
-//     registrationForm.setAttribute("style","transform:: translate(0);");
-// };
-
-// loginHere.onclick=()=>{
-//     loginForm.removeAttributeAttribute("style");
-//     registrationForm.setAttribute("style");
-// };
-
 const modalContainer = document.querySelector(".modal-container"); // Fixed typo: "modal-conatiner" to "modal-container"
 const modalLogin = document.getElementById("modalLogin"); // Fixed typo: remove the dot
 const create = document.getElementById("create");
@@ -49,6 +24,6 @@ create.onclick = () => {
 };
 
 loginHere.onclick = () => {
-    loginForm.style.transform = "translate(0)";
-    registrationForm.style.transform = "translate(500px)"; // This was missing, assuming you want to move the registration form out of view.
+    loginForm.style.transform = ("style");
+    registrationForm.style.transform = ("style"); // This was missing, assuming you want to move the registration form out of view.
 };
