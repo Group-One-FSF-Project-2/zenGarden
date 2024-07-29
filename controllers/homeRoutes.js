e272112fdd0c3396b6b05f217a33f30e2231213a
mjurek1 <jurekmichael0@gmail.com>
2024-07-29 14:10:53 -0500

    protected routes with auth

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { Gardenplot, User } = require('../models');
 const withAuth = require('../utils/auth');
+
 
 // TESTING WITH PARAMETERS - Scott 28Jul
 router.get('/', (req, res) => {
@@ -9,26 +11,97 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/plots', (req, res) => {
-  res.render('allPlots');
-});
+// router.get('/plots', (req, res) => {
+//   res.render('allPlots');
+// });
 
 router.get('/plots/new', (req, res) => {
   res.render('newPlot');
 });
 
 
-router.get('/plots/:id', (req, res) => {
-  // we need to get the plot information from the database.
-  res.render('singlePlot', {
-    plotId: 3,
-    plot_name: 'My new garden',
-  });
+// router.get('/plots/:id', (req, res) => {
+//   // we need to get the plot information from the database.
+//   res.render('singlePlot', {
+//     plotId: 3,
+//     plot_name: 'My new garden',
+//   });
+// });
+
+// // FOR TESTING  localhost:3001/garden
+// router.get('/garden', (req, res) => {
+//   res.json('Hello World');
+// });
+
+router.get('/', async (req, res) => {
+  try {
+    const plotData = await Gardenplot.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['user_name'],
+        },
+      ],
+    });
+
+    const plots = plotData.map((plot) => plot.get({ plain: true }));
+
+    res.render('allPlots', {
+      plots,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// FOR TESTING  localhost:3001/garden
-router.get('/garden', (req, res) => {
-  res.json('Hello World');
+router.get('/plots/:id', withAuth, async (req, res) => {
+  try {
+    const plotData = await Gardenplot.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['user_name'],
+        },
+      ],
+    });
+
+    const plot = plotData.get({ plain: true });
+
+    res.render('allPlots', {
+      ...plot,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/plots', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: {exclude: ['password'] },
+      include: [{ model: Gardenplot}],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('allPlots', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/plots');
+    return;
+  }
+
+  res.render('home');
 });
 
 module.exports = router;
