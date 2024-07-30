5f5734e3036e2a7f97736b0f07372eaf39a39abc
mjurek1 <jurekmichael0@gmail.com>
2024-07-29 19:19:11 -0500

    putting auth back in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,107 +1,107 @@
 const router = require('express').Router();
-// const { Gardenplot, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const { Gardenplot, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 
-// TESTING WITH PARAMETERS - Scott 28Jul
-router.get('/', (req, res) => {
-  // what shoult be displayed on the homepage?????
-  res.render('home', {
-    logged_in: req.session.logged_in,
-  });
-});
+// // TESTING WITH PARAMETERS - Scott 28Jul
+// router.get('/', (req, res) => {
+//   // what shoult be displayed on the homepage?????
+//   res.render('home', {
+//     logged_in: req.session.logged_in,
+//   });
+// });
 
-router.get('/plots', (req, res) => {
-  res.render('allPlots');
-});
+// router.get('/plots', (req, res) => {
+//   res.render('allPlots');
+// });
 
-router.get('/plots/new', (req, res) => {
-  res.render('newPlot');
-});
+// router.get('/plots/new', (req, res) => {
+//   res.render('newPlot');
+// });
 
 
-router.get('/plots/:id', (req, res) => {
-  // we need to get the plot information from the database.
-  res.render('singlePlot', {
-    plotId: 3,
-    plot_name: 'My new garden',
-  });
-});
+// router.get('/plots/:id', (req, res) => {
+//   // we need to get the plot information from the database.
+//   res.render('singlePlot', {
+//     plotId: 3,
+//     plot_name: 'My new garden',
+//   });
+// });
 
 // // FOR TESTING  localhost:3001/garden
 // router.get('/garden', (req, res) => {
 //   res.json('Hello World');
 // });
 
-// router.get('/', async (req, res) => {
-//   try {
-//     const plotData = await Gardenplot.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['user_name'],
-//         },
-//       ],
-//     });
+router.get('/', async (req, res) => {
+  try {
+    const plotData = await Gardenplot.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['user_name'],
+        },
+      ],
+    });
 
-//     const plots = plotData.map((plot) => plot.get({ plain: true }));
+    const plots = plotData.map((plot) => plot.get({ plain: true }));
 
-//     res.render('allPlots', {
-//       plots,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('home', {
+      plots,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/plots/:id', withAuth, async (req, res) => {
-//   try {
-//     const plotData = await Gardenplot.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['user_name'],
-//         },
-//       ],
-//     });
+router.get('/plots/:id', withAuth, async (req, res) => {
+  try {
+    const plotData = await Gardenplot.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['user_name'],
+        },
+      ],
+    });
 
-//     const plot = plotData.get({ plain: true });
+    const plot = plotData.get({ plain: true });
 
-//     res.render('allPlots', {
-//       ...plot,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('singlePlot', {
+      ...plot,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/plots', withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: {exclude: ['password'] },
-//       include: [{ model: Gardenplot}],
-//     });
+router.get('/plots', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: {exclude: ['password'] },
+      include: [{ model: Gardenplot}],
+    });
 
-//     const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true });
 
-//     res.render('allPlots', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('singlePlot', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/login', (req, res) => {
-//   if (req.session.logged_in) {
-//     res.redirect('/plots');
-//     return;
-//   }
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/plots');
+    return;
+  }
 
-//   res.render('home');
-// });
+  res.render('home');
+});
 
 module.exports = router;
