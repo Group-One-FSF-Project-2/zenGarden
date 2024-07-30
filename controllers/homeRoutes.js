68c1a62394d15cb8271c6aa94622fb0eeea90528
smrsun4 <smrsun4@gmail.com>
2024-07-29 21:23:56 -0400

    test

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,13 +17,13 @@ router.get('/plots/new', (req, res) => {
 });
 
 
-router.get('/plots/:id', (req, res) => {
-  // we need to get the plot information from the database.
-  res.render('singlePlot', {
-    plotId: 3,
-    plot_name: 'My new garden',
-  });
-});
+// router.get('/plots/:id', (req, res) => {
+//   // we need to get the plot information from the database.
+//   res.render('singlePlot', {
+//     plotId: 3,
+//     plot_name: 'My new garden',
+//   });
+// });
 
 // FOR TESTING  localhost:3001/garden
 // router.get('/garden', (req, res) => {
